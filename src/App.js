82c55a97f2017b9eecfcb82c4423937405880aaa
CatsAreMLG <joshuadgon@gmail.com>
2020-04-14 23:59:29 -0500

    Add app styling

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+
+import flowers from './data/flowers.json'
 
 function App() {
+  let grid = Object.keys(flowers.tulips.flags).map((flower) => {
+    return (
+      <div
+        className="garden_flower"
+        style={{ background: flowers.tulips.flags[flower].color }}>
+        <span className="garden_flower_text">
+          {flowers.tulips.flags[flower].flag}
+        </span>
+      </div>
+    )
+  })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="banner">
+        <h1>Rosy Garden</h1>
+      </div>
+      <div className="garden">{grid.splice(0, 25)}</div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
